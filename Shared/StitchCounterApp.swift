e050bebf8248f3dd1073d4d51ee3d2a27616a484
Zheng <aheze@getfind.app>
2021-09-06 10:02:29 -0700

    Initial Commit

---
+++ Shared/StitchCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StitchCounterApp.swift
+//  Shared
+//
+//  Created by Zheng on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct StitchCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
